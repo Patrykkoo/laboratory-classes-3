2b509bb92a74e4bf0489dca9ccde63d139bf392b
Patrykkoo <patryk.chojnacki02@gmail.com>
2025-04-08 18:40:47 +0200

    Edycja server.js

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const bodyParser = require("body-parser");
 
 const { PORT } = require("./config");
 const logger = require("./utils/logger");
-const productRoutes = require("./routing/product");
+const productsRoutes = require("./routing/products");
 const logoutRoutes = require("./routing/logout");
 const killRoutes = require("./routing/kill");
 const homeRoutes = require("./routing/home");
 const { STATUS_CODE } = require("./constants/statusCode");
 const getFileFromAbsolutePath = require("./utils/getFileFromAbsolutePath");
-const { get } = require("http");
+const { HOME_LINK } = require("./constants/navigation");
 
 const app = express();
 
@@ -29,16 +29,21 @@ app.use((request, _response, next) => {
   next();
 });
 
-app.use("/product", productRoutes);
+app.use("/products", productsRoutes);
 app.use("/logout", logoutRoutes);
 app.use("/kill", killRoutes);
 app.use(homeRoutes);
+
 app.use((request, response) => {
   const { url } = request;
 
-  response
-    .status(STATUS_CODE.NOT_FOUND)
-    .sendFile(path.join(__dirname, "./views", "404.html"));
+  response.status(STATUS_CODE.NOT_FOUND).render("404", {
+    headTitle: "Page Not Found",
+    path: url,
+    menuLinks: [HOME_LINK],
+    activeLinkPath: url,
+  });
+
   logger.getErrorLog(url);
 });
 
